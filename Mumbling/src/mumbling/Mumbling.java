8f53dbdbfbc467ee4f65c5ed59b657781f69c8e3
TrollerX <trollerx@somemail.com>
2017-09-12 14:57:22 +0200

    ;

--- Mumbling/src/mumbling/Mumbling.java
+++ Mumbling/src/mumbling/Mumbling.java
@@ -31,4 +31,19 @@ public class Mumbling {
         return accum.substring(0, accum.length() - 1);
     }
 
+//    public static String accum(String s) {
+//        StringBuilder bldr = new StringBuilder();
+//        int i = 0;
+//        for (char c : s.toCharArray()) {
+//            if (i > 0) {
+//                bldr.append('-');
+//            }
+//            bldr.append(Character.toUpperCase(c));
+//            for (int j = 0; j < i; j++) {
+//                bldr.append(Character.toLowerCase(c));
+//            }
+//            i++;
+//        }
+//        return bldr.toString();
+//    }
 }
