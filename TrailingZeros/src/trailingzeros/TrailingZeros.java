af60fde27bc5f51cea94cf92953edafc632ab007
TrollerX <trollerx@somemail.com>
2017-09-12 13:55:23 +0200

    .

---
+++ TrailingZeros/src/trailingzeros/TrailingZeros.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trailingzeros;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author TrollerX
+ */
+public class TrailingZeros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(zeros(12));
+    }
+
+    public static int zeros(int n) {
+        // your beatiful code here
+        BigInteger factorial = factorial(n);
+//        System.out.println("Factorial of: " + n + " is: " + factorial);
+        String fStr = factorial.toString();
+//        System.out.println("String representation: " + fStr);
+//        System.out.println("Number of digits: " + fStr.length());
+        int i = fStr.length() - 1;
+        int zeros = 0;
+        while (fStr.charAt(i) == '0') {
+            zeros++;
+            i--;
+        }
+        return zeros;
+    }
+
+    public static BigInteger factorial(int n) {
+        BigInteger factorial = BigInteger.valueOf(1);
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        return factorial;
+    }
+
+}
